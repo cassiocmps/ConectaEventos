49f1c91cf8c29ed659d52d542d8c322cb453f129
cassiocmps <cassio.szcampos@gmail.com>
2023-11-20 14:32:34 -0300

    [UPDATE] get all produtos

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ConectaEventos.Services;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly ProdutoService _produtoService;
+
+    public ProdutosController(ProdutoService produtoService)
+    {
+        _produtoService = produtoService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllProdutos()
+    {
+        var produtos = _produtoService.GetAllProdutos();
+
+        if (produtos != null && produtos.Count > 0)
+        {
+            return Ok(produtos);
+        }
+
+        return NotFound("No Produtos found.");
+    }
+}
---
+++ Services/ProdutoService.cs
@@ -0,0 +1,23 @@
+﻿using ConectaEventos.Data;
+using ConectaEventos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConectaEventos.Services
+{
+    public class ProdutoService
+    {
+        private readonly ConectaEventosContext _context;
+
+        public ProdutoService(ConectaEventosContext context)
+        {
+            _context = context;
+        }
+
+        public List<Produto> GetAllProdutos()
+        {
+            return _context.Produtos
+                .AsNoTracking()
+                .ToList();
+        }
+    }
+}
